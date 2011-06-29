bdca762f85f3a3e756ab6ba853790b63f4e1484e
Michael JasonSmith <mpj17@onlinegroups.net>
2011-06-29 12:27:41 +1000

    Adding a page to allow people to change the site name.

---
+++ gs/site/change/name/changename.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from gs.content.form.form import SiteForm
+from gs.content.form.select import select_widget
+from gs.content.form.utils import enforce_schema
+from interfaces import IGSSiteName
+
+class ChangeName(SiteForm):
+    label = u'Change the Site Name'
+    pageTemplateFileName = 'browser/templates/changename.pt'
+    template = ZopeTwoPageTemplateFile(pageTemplateFileName)
+    form_fields = form.Fields(IGSSiteName, render_context=False)
+
+    def __init__(self, context, request):
+        SiteForm.__init__(self, context, request)
+
+    def setUpWidgets(self, ignore_request=False):
+        data = {'name': self.siteInfo.name,}
+        self.widgets = form.setUpWidgets(
+            self.form_fields, self.prefix, self.context,
+            self.request, form=self, data=data,
+            ignore_request=ignore_request)
+
+    @form.action(label=u'Change', failure='handle_change_action_failure')
+    def handle_change(self, action, data):
+        oldName = self.siteInfo.name
+        self.siteInfo.siteObj.manage_changeProperties(title=data['name'])
+        self.status = u'The of this site has been '\
+            'changed to <q>%s</a> from <q>%s</q>.' % \
+            (data['name'], oldName)
+        assert type(self.status) == unicode
+        
+    def handle_change_action_failure(self, action, data, errors):
+        if len(errors) == 1:
+            self.status = u'<p>There is an error:</p>'
+        else:
+            self.status = u'<p>There are errors:</p>'
+        assert type(self.status) == unicode
+
---
+++ gs/site/change/name/interfaces.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from zope.interface import Interface
+from zope.schema import TextLine
+
+class IGSSiteName(Interface):
+    name = TextLine(title=u'Site Name',
+      description=u'The name of your site. It will be used in the '\
+        u'body of pages, email messages, and at the top of every page.',
+      required=True)
+
